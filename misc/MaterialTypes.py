15677f05873711ca106ca9767bd0541c223660f3
Philly <lfilippelli5@gmail.com>
2024-11-03 11:15:24 -0500

    Table creation script plus DB file and name reference classes

---
+++ misc/MaterialTypes.py
@@ -0,0 +1,23 @@
+class MaterialTypes:
+
+    # Defines plausible material names by the two largest subtypes
+
+    FDM = [
+        ("PLA",),
+        ("Silk PLA",),
+        ("ABS",),
+        ("Nylon",),
+        ("PAHT CF",),
+        ("PETG",),
+        ("ASA",),
+        ("TPU",),
+    ]
+
+    SLA = [
+        ("Smoky Black",),
+        ("Strong X",),
+        ("ABS Like V2",),
+        ("TGM",),
+        ("High Translucent",),
+        ("Casting",)
+    ]
