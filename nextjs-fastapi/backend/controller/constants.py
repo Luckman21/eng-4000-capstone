ac1917c230aad6a02012119e85b51d0d4f1fac0c
Luckman21 <luckmaan21@gmail.com>
2025-01-07 11:01:03 -0500

    Create constants class
    Created a class for global constants for our backend.  This should reduce code duplication and ensure parity across the backend, in case any of these values should change in the future.

---
+++ nextjs-fastapi/backend/controller/constants.py
@@ -0,0 +1,3 @@
+# A class of constants for the backend.  Classes should refer to this class when using globally defined constants to ensure parity.
+DATABASE_URL = 'sqlite:///nextjs-fastapi/db/capstone_db.db' # The URL for our database
+THRESHOLD = 50  # 50g mass threshold for materials
