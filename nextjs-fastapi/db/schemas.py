f2910a512aad87a2e43877349f4f31f9637f9a04
Philly <lfilippelli5@gmail.com>
2025-01-16 13:04:37 -0500

    Add schema

--- nextjs-fastapi/db/schemas.py
+++ nextjs-fastapi/db/schemas.py
@@ -10,3 +10,20 @@ class MaterialSchema(BaseModel):
 
     class Config:
         orm_mode = True
+
+class MaterialTypeSchema(BaseModel):
+    id: int
+    type_name: str
+
+    class Config:
+        orm_mode = True
+class UserSchema(BaseModel):
+    id: int
+    username: str
+    password: str
+    email: str
+    user_type_id: int
+
+    class Config:
+        orm_mode = True
+
