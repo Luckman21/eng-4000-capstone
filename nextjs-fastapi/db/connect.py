5c775eb0290aea2c665d093b26edc056dc428c85
Philly <lfilippelli5@gmail.com>
2025-01-19 18:06:40 -0500

    Run DB script

--- nextjs-fastapi/db/connect.py
+++ nextjs-fastapi/db/connect.py
@@ -3,6 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from backend.controller import constants
 
-DATABASE_URL = constants.DATABASE_URL
+import os
+
+if os.getenv("CI"):  # If in CI environment
+    DATABASE_URL = "sqlite:///:memory:"  # Use in-memory database
+else:
+    DATABASE_URL = constants.DATABASE_URL_TEST  # Local file-based DB
+
+
+DATABASE_URL = constants.DATABASE_URL_TEST
 engine = create_engine(DATABASE_URL, echo=True)
 session = sessionmaker(bind=engine)
