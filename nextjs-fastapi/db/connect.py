f77ccc310598739605926fb441f9bec63b377032
Philly <lfilippelli5@gmail.com>
2025-01-20 00:42:39 -0500

    I think this broke the integration tests IV

--- nextjs-fastapi/db/connect.py
+++ nextjs-fastapi/db/connect.py
@@ -5,11 +5,9 @@ from backend.controller import constants
 
 import os
 
-if os.getenv("CI"):  # If in CI environment
-    DATABASE_URL = "sqlite:///:memory:"  # Use in-memory database
-else:
-    DATABASE_URL = constants.DATABASE_URL_TEST  # Local file-based DB
 
+
+DATABASE_URL = constants.DATABASE_URL_TEST
 
 engine = create_engine(DATABASE_URL, echo=True)
 session = sessionmaker(bind=engine)
