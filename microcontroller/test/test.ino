731b7b1e250929e7cbb607024a5b611d6caf70db
Luckman21 <luckmaan21@gmail.com>
2025-01-20 20:06:47 -0500

    Allow Arduino to upload data independently
    Allows the Arduino to continue without waiting for the serial monitor to connect.  This means that, with power, the Arduino will independently begin recording data and publishing once connected to the internet and a connection is established to a broker.

--- microcontroller/test/test.ino
+++ microcontroller/test/test.ino
@@ -31,9 +31,6 @@ unsigned long prev_ms = 0;
 
 void setup() {
   Serial.begin(9600);
-  while(!Serial) {
-    ; // wait for serial port to connect.  Needed for native USB port only
-  }
 
   // set up DHT sensor
   dht.begin();
