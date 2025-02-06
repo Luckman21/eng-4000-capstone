5c37d9651563f5a1004d0d39441842bcfed47b08
Philly <lfilippelli5@gmail.com>
2025-02-06 17:18:42 -0500

    Temp Password Randomizer

---
+++ nextjs-fastapi/backend/service/TempPasswordRandomizeService.py
@@ -0,0 +1,15 @@
+import secrets
+import string
+import random
+
+
+
+def create_temp_password() -> str:
+    length = random.randrange(8,16)
+
+    alphabet = string.ascii_letters + string.digits + string.punctuation
+
+    temp_password = ''.join(secrets.choice(alphabet) for i in range(length))
+
+    return temp_password
+
