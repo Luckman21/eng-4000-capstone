3d18e88b1c71f5553df5197674ad85c1f5ab40eb
Philly <lfilippelli5@gmail.com>
2025-02-17 16:02:39 -0500

    Mailer + test

---
+++ nextjs-fastapi/backend/service/mailer/SaleMailer.py
@@ -0,0 +1,14 @@
+from backend.service.mailer.MailerService import _MailerService
+
+class SaleMailer:
+
+    def __init__(self, from_addr):
+        self.from_addr = from_addr
+        self.client = _MailerService()
+
+    def send_notification(self, to_addr, sale_materials):
+        subject = "Pantheon Inventory Management: Inventory Sale Found"
+        body = f"Well met, the following inventory items have sales or special prices: \n{sale_materials}"
+
+        # Send the email using the private _MailerService
+        return self.client.send_email(to_addr, subject, body, self.from_addr)
