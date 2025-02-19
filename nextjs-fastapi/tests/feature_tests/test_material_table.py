541b933f60ed1e3720ef3872866f694eb049e0b1
Philly <lfilippelli5@gmail.com>
2025-02-19 13:54:13 -0500

    Try to fix timeout

--- nextjs-fastapi/tests/feature_tests/test_material_table.py
+++ nextjs-fastapi/tests/feature_tests/test_material_table.py
@@ -65,7 +65,7 @@ def test_material_table_buttons(driver, login):
 
     # Check each row for the presence of two SVG elements
     for index, row in enumerate(rows):
-        WebDriverWait(row, 30).until(EC.visibility_of_all_elements_located((By.TAG_NAME, "svg")))
+        WebDriverWait(row, 50).until(EC.visibility_of_all_elements_located((By.TAG_NAME, "svg")))
         svg_elements = row.find_elements(By.TAG_NAME, "svg")
 
         # Assert that each row has exactly 2 SVGs (or adjust as necessary)
