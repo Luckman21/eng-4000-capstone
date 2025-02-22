e93be947f589186e06a7c65bf198131af3673cd7
Philly <lfilippelli5@gmail.com>
2025-02-22 12:27:58 -0500

    Removed useless comments

--- nextjs-fastapi/tests/feature_tests/test_sale_scraper.py
+++ nextjs-fastapi/tests/feature_tests/test_sale_scraper.py
@@ -24,10 +24,10 @@ def driver():
 
 def test_scrape_amazon_page_for_sale_found(driver):
     # Test against a real URL (make sure the page you're testing on has a sale)
-    url = 'https://www.amazon.ca/ELEGOO-Filament-Dimensional-Accuracy-Compatible/dp/B0BM95MYNX/ref=sxin_15_pa_sp_search_thematic_sspa?content-id=amzn1.sym.46621be6-fabe-4126-8501-d32c96c42a24%3Aamzn1.sym.46621be6-fabe-4126-8501-d32c96c42a24&cv_ct_cx=PLA&keywords=PLA&pd_rd_i=B0BM95MYNX&pd_rd_r=801386cf-36e0-470c-aa3d-fd4084381423&pd_rd_w=usjfA&pd_rd_wg=A4vii&pf_rd_p=46621be6-fabe-4126-8501-d32c96c42a24&pf_rd_r=TQBGZJC1XMJR5CW8WMAX&qid=1739823127&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sr=1-1-acb80629-ce74-4cc5-9423-11e8801573fb-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9zZWFyY2hfdGhlbWF0aWM&th=1'  # Replace with an actual Amazon product URL with a sale
+    url = 'https://www.amazon.ca/ELEGOO-Filament-Dimensional-Accuracy-Compatible/dp/B0BM95MYNX/ref=sxin_15_pa_sp_search_thematic_sspa?content-id=amzn1.sym.46621be6-fabe-4126-8501-d32c96c42a24%3Aamzn1.sym.46621be6-fabe-4126-8501-d32c96c42a24&cv_ct_cx=PLA&keywords=PLA&pd_rd_i=B0BM95MYNX&pd_rd_r=801386cf-36e0-470c-aa3d-fd4084381423&pd_rd_w=usjfA&pd_rd_wg=A4vii&pf_rd_p=46621be6-fabe-4126-8501-d32c96c42a24&pf_rd_r=TQBGZJC1XMJR5CW8WMAX&qid=1739823127&sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D&sr=1-1-acb80629-ce74-4cc5-9423-11e8801573fb-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9zZWFyY2hfdGhlbWF0aWM&th=1'
     assert scrape_amazon_page_for_sale(url, driver) is True
 
 def test_scrape_digitmaker_page_for_sale_found(driver):
     # Test against a real URL (make sure the page you're testing on has a sale)
-    url = 'https://www.digitmakers.ca/collections/esun-filaments/products/esun-emarble-pla-filament-1-75mm-1kg'  # Replace with an actual Amazon product URL with a sale
+    url = 'https://www.digitmakers.ca/collections/esun-filaments/products/esun-emarble-pla-filament-1-75mm-1kg'
     assert scrape_digitkey_page_for_sale(url, driver) is True
