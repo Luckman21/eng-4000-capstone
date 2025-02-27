77bc5cb34182681f5a9301daf96f154d5b25de3e
Philly <lfilippelli5@gmail.com>
2025-02-26 20:26:38 -0500

    First change - test

---
+++ nextjs-fastapi/tests/feature_tests/test_user_access.py
@@ -0,0 +1,47 @@
+import subprocess
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import Remote
+import chromedriver_autoinstaller
+import re
+import os
+from tests.feature_tests.login_helper import log_admin_in, log_super_admin_in
+
+
+
+TEST_URL = "http://127.0.0.1:3000/users"
+
+@pytest.fixture(scope="module")
+def driver():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # This means you won't see the actual icon
+    chrome_options.add_argument("--disable-gpu") # Disable GPU acceleration (required in headless mode)
+    chrome_options.add_argument("--no-sandbox")  # Might help in some environments
+    # This will change depending on your driver
+
+    # Automatically installs the correct ChromeDriver version for your installed Chrome
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome(options=chrome_options)
+
+    yield driver
+    driver.quit()
+
+@pytest.fixture(scope="module")
+def login(driver):
+    log_admin_in(driver)
+    time.sleep(3)
+
+
+def admin_cannot_access_users(driver, login):
+    driver.get(TEST_URL)
+    no_access = WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "text-red-500 text-xl font-bold"))
+    )
+
+    assert no_access.text == "No Access"
