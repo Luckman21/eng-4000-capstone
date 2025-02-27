9bf79d911e505c13e8bf8a8a07f94ac4ef1574a1
Philly <lfilippelli5@gmail.com>
2025-02-26 20:40:42 -0500

    First change - test II

--- nextjs-fastapi/tests/feature_tests/test_user_access.py
+++ nextjs-fastapi/tests/feature_tests/test_user_access.py
@@ -20,7 +20,7 @@ TEST_URL = "http://127.0.0.1:3000/users"
 @pytest.fixture(scope="module")
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument("--headless") # This means you won't see the actual icon
+    #chrome_options.add_argument("--headless") # This means you won't see the actual icon
     chrome_options.add_argument("--disable-gpu") # Disable GPU acceleration (required in headless mode)
     chrome_options.add_argument("--no-sandbox")  # Might help in some environments
     # This will change depending on your driver
@@ -38,10 +38,13 @@ def login(driver):
     time.sleep(3)
 
 
-def admin_cannot_access_users(driver, login):
+def test_admin_cannot_access_users(driver, login):
     driver.get(TEST_URL)
-    no_access = WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "text-red-500 text-xl font-bold"))
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located((By.TAG_NAME, "div"))
     )
 
+    no_access = driver.find_elements(By.TAG_NAME, 'div')[-1]
+
     assert no_access.text == "No Access"
+
