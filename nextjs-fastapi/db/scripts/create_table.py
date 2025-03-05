69c4927d9c8687d934fcefe43fbfaaef0ab66497
Philly <lfilippelli5@gmail.com>
2025-03-05 17:26:17 -0500

    Change null shelf redundancy (shit can be outside of a shelf too)

--- nextjs-fastapi/db/scripts/create_table.py
+++ nextjs-fastapi/db/scripts/create_table.py
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS materials (
     supplier_link TEXT NOT NULL, 
     mass DOUBLE PRECISION NOT NULL,
     material_type_id INTEGER NOT NULL REFERENCES material_types(id) ON DELETE CASCADE,
-    shelf_id INTEGER NOT NULL REFERENCES shelfs(id) ON DELETE CASCADE
+    shelf_id INTEGER REFERENCES shelfs(id) ON DELETE CASCADE
 )
 ''')
 
