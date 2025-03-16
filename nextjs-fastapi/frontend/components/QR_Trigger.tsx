6fe8d5db387b2ee952fc030d8fb60037d1895f6b
Tharuveen Raveendran <tharuveenravi@hotmail.ca>
2025-03-16 19:44:16 -0400

    Updated Styling changes

--- nextjs-fastapi/frontend/components/QR_Trigger.tsx
+++ nextjs-fastapi/frontend/components/QR_Trigger.tsx
@@ -63,8 +63,8 @@ const QR_Trigger = () => {
     if (error) return <p className="text-red-500">{error}</p>;
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen p-4 bg-gray-900 text-white">
-          <Card className="w-full max-w-md p-6 rounded-lg shadow-lg bg-gray-800">
+        <div className="flex flex-col items-center pt-30 px-4 bg-gray-900 text-white" style={{ marginTop: "150px" }}>
+          <Card className="w-full max-w-md p-15 rounded-lg shadow-lg bg-gray-800 mt-4">
           <CardBody className="text-center">
             <h2 className="text-2xl font-bold">{material?.material_type_name} - {material?.material.colour}</h2>
             <p className="text-sm text-gray-400">Shelf: {material?.material.shelf_id || "N/A"}</p>
@@ -79,14 +79,14 @@ const QR_Trigger = () => {
                     className="mb-4"
                 />
             </p>
-            <div className="flex justify-center gap-4 mt-6">
-                <Button color="primary" onPress={handleReplenish}>
+            <div className="flex justify-center items-center mt-6">
+                <Button color="primary" style={{marginRight: "50px", marginTop: "5px"}} onPress={handleReplenish}>
                     Add Mass
                 </Button>
-                <Button color="primary" onPress={handleConsume}>
+                <Button color="primary" style={{marginTop: "5px"}} onPress={handleConsume}>
                     Remove Mass
                 </Button>
-                <Button color="danger" variant="flat" onPress={() => router.push("/inventory")}>
+                <Button color="danger" variant="flat" style={{marginLeft: "50px", marginTop: "5px"}} onPress={() => router.push("/inventory")}>
                     Cancel
                 </Button>
             </div>
