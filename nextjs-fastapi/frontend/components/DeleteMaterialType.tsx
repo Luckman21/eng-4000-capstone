ad9e1e3511e3edf9b68010fe2fd658b6ec75ddf6
Paras Kumar <90861109+ParasK02@users.noreply.github.com>
2025-04-02 21:37:48 -0400

    Create DeleteMaterialType.tsx

---
+++ nextjs-fastapi/frontend/components/DeleteMaterialType.tsx
@@ -0,0 +1,75 @@
+"use client";
+import React from 'react'
+import { useEffect, useState } from "react";
+
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button
+} from "@heroui/react";
+
+type DeletePopupTypes = {
+  item: { id: number} | null;
+  isOpen: boolean;
+  onOpenChange: () => void;
+  onDelete: (id: number) => void;
+  itemType: string;
+};
+
+const DeleteMaterialType: React.FC<DeletePopupTypes> = ({ item, isOpen, onOpenChange, onDelete, itemType}) => {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  
+
+  const handleDelete = async () => {
+    if (!item) return;
+    try {
+      // send delete request to backend
+      const response = await fetch(`/${itemType}/${item.id}` , {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+
+      if (!response.ok) throw new Error("Failed to delete material type");
+
+      onDelete(item.id);
+      onOpenChange();
+    } catch (error) {
+
+      // Extract "detail" if it exists
+      const errorMessage = "Delete material type attempt failed. Please try again.";
+      setErrorMessage(errorMessage);
+      console.error("Error deleting material type:", error);
+    }
+  };
+
+  return (
+    <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center" backdrop="opaque">
+      <ModalContent>
+        <ModalHeader className="flex flex-col gap-1">
+          Delete Item
+        </ModalHeader>
+        <ModalBody>
+          Are you sure you want to delete this material type? This action cannot be undone.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="danger" variant="flat" onPress={onOpenChange}>
+            Close
+          </Button>
+          <Button color="primary" onPress={handleDelete}>
+            Delete
+          </Button>
+                  {errorMessage && (
+            <p className="text-danger bg-red-100 border border-red-400 text-sm mt-2 p-2 rounded">
+        {errorMessage}
+            </p>
+        )}
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+export default DeleteMaterialType;
