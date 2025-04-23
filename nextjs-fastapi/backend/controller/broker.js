cef28d586fe15b9f86880d645d99d8e29d4af4e3
Luckman21 <luckmaan21@gmail.com>
2025-04-23 11:56:53 -0400

    Allow Connections Externally
    Adding listen to IP address 0.0.0.0 sets the broker up to listen to external devices.

--- nextjs-fastapi/backend/controller/broker.js
+++ nextjs-fastapi/backend/controller/broker.js
@@ -2,9 +2,10 @@ const aedes = require('aedes')();
 const net = require('net');
 const server = net.createServer(aedes.handle);
 
-server.listen(1883, () => {
-  console.log('Aedes MQTT broker running on port 1883');
+server.listen(1883, '0.0.0.0', () => {
+  console.log('Aedes MQTT broker running on port 1883 and accessible externally');
 });
+
 
 aedes.on('client', client => {
   console.log(`[BROKER] Client connected: ${client.id}`);
